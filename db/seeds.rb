596fd3fe70a6671974c019ea8667c3582ba59d55
mandarandriam <mandarandriam@gmail.com>
2018-08-27 16:00:27 +0300

    aaa

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,45 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+Item.create (title:"brown cat - head", description:"1600x900", price: 15.00, image_url:"http://iwallpapers.free.fr/upload/2012/08/17/20120817111810-cfe634b0.jpg") 
+Item.create (title:"grey cat", description:"1920x1080" , price: 20.00, image_url:"http://www.fond-ecran-hd.net/pc-driver/1212.jpg") 
+Item.create (title:"grey cat - blue eyes", description:"1920x1200" , price: 22.00, image_url:"http://www.fond-ecran-hd.net/pc-driver/1588.jpg") 
+Item.create (title:"grumpy cat", description:"1920x1080" , price: 20.00, image_url:"http://www.v3wall.com/wallpaper/1920_1080/1001/1920_1080_20100105094209331104.jpg") 
+Item.create (title:"autumn cat", description:"1600x1000" , price: 18.00, image_url:"https://1.bp.blogspot.com/-KNL9kuNQjrM/WDllBoGbiJI/AAAAAAAADLk/LaEU4G1InW4dJaZ9VuvHb4jv9TcdpIhZgCLcB/s1600/Fond%2Bd%2527ecran-%2Bchat%2B05.jpg") 
+Item.create (title:"siamese cat", description:"1920x1200" , price: 18.00, image_url:"http://www.fond-ecran-hd.net/pc-driver/474.jpg") 
+Item.create (title:"cozy cat", description:"1200x1920" , price: 18.00, image_url:"http://www.chezmaya.com/wp-content/uploads/2017/04/fond-d-ecran-chaton-chaise-longue-vacance.jpg") 
+Item.create (title:"christmas bro-cat", description:"1200x1920" , price: 18.00, image_url:"http://i67.servimg.com/u/f67/12/97/01/02/laba_w24.jpg") 
+Item.create (title:"playing cat", description:"1200x1600" , price: 16.00, image_url:"https://s-media-cache-ak0.pinimg.com/originals/e2/47/6d/e2476d9e1524ba9c903f33376f173857.jpg") 
+Item.create (title:"flower cat", description:"768x1024" , price: 13.00, image_url:"http://www.chezmaya.com/wp-content/uploads/2017/01/fond_ecran_1215-1024x768.jpg") 
+Item.create (title:"siamese cat 2", description:"710x1023" , price: 12.00, image_url:"http://mesptitesimages.m.e.pic.centerblog.net/d9a98b90.gif") 
+Item.create (title:"snowy cat", description:"640x1024" , price: 10.00, image_url:"https://goodies.pcastuces.com/vignettes/9330_l.jpg") 
+Item.create (title:"twins brown cat", description:"541x722" , price: 8.00, image_url:"http://www.weesk.com/wallpaper/animaux/chats-chatons/chats-chatons-804/chats-chatons-804-720px.jpg") 
+Item.create (title:"snow tiger cat", description:"541x722" , price: 8.00, image_url:"http://www.weesk.com/wallpaper/animaux/chats-chatons/chats-chatons-36/chats-chatons-36-720px.jpg") 
+Item.create (title:"lazy cat", description:"469x750" , price: 6.00, image_url:"http://img.hebus.com/hebus_2011/11/13/preview/111113155654_46.jpg") 
+Item.create (title:"tiger cat", description:"1080x1920" , price: 20.00, image_url:"http://www.1zoom.me/big2/441/283571-alexfas01.jpg") 
+Item.create (title:"black cat - head", description:"1200x1920" , price: 22.00, image_url:"http://images.annuaire-animaux.net/fonds-ecran/maxi/chat-chartreux.jpg") 
+Item.create (title:"black cat - center", description:"1440x2560" , price: 25.00, image_url:"http://www.fond-ecran-hd.net/pc-driver/1321.jpg") 
+Item.create (title:"bro-cats", description:"768x1024" , price: 13.00, image_url:"http://www.paradis-des-chats.com/medias/images/fondecran08.jpg") 
+Item.create (title:"cute cat", description:"768x1024" , price: 13.00, image_url:"http://www.unesourisetmoi.info/wallpaper_7/images/chat-et-ours.jpg")  
+# 1600x900
+# 1920x1080
+# 1920x1200
+# 1920x1080
+# 1600x1000
+# 1920x1200
+# 1200x1920
+# 1200x1920
+# 1200x1600
+# 768x1024
+# 710x1023
+# 640x1024
+# 541x722
+# 541x722
+# 469x750
+# 1080x1920
+# 1200x1920
+# 1440x2560
+# 768x1024
 #
-# Examples:
+# 
+# 
+# 
+# 
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
